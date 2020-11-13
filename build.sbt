48c41469c53e6fb278dcf297030ac3a816b9a0a4
Gabriel Plassard <gabriel.plassard@gmail.com>
2020-11-13 23:50:11 +0100

    Template pour récupérer la consommation courante

--- build.sbt
+++ build.sbt
@@ -7,4 +7,5 @@ scalaVersion := "2.13.3"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-stream-contrib" % "0.11",
   "com.lightbend.akka" %% "akka-stream-alpakka-dynamodb" % "2.0.2",
+  "software.amazon.awssdk" % "cloudwatch" % "2.15.26"
 )
--- src/main/scala/fr/gplassard/akkastreamdynamicthrottle/Main.scala
+++ src/main/scala/fr/gplassard/akkastreamdynamicthrottle/Main.scala
@@ -8,10 +8,16 @@ import akka.stream.Attributes.LogLevels
 import akka.stream.alpakka.dynamodb.scaladsl.DynamoDb
 import akka.stream.contrib.TokenThrottle
 import akka.stream.scaladsl.{Sink, Source}
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient
+import software.amazon.awssdk.services.cloudwatch.model.{GetMetricDataRequest, MetricDataQuery, MetricStat}
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
 import software.amazon.awssdk.services.dynamodb.model.DescribeTableRequest
 
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.DurationInt
+import scala.jdk.CollectionConverters._
+import scala.jdk.FutureConverters._
+
 import scala.language.postfixOps
 
 object Main extends App {
@@ -19,21 +25,41 @@ object Main extends App {
   implicit val loggingAdapter = system.log
   import system.dispatcher
   implicit val client: DynamoDbAsyncClient = DynamoDbAsyncClient.builder().build()
+  implicit val cloudWatch: CloudWatchAsyncClient = CloudWatchAsyncClient.builder().build()
 
   val tableName = "tableName"
+
+  val currentThrottle = Await.result(DynamoDb.single(DescribeTableRequest.builder().tableName(tableName).build()), 5 seconds)
+    .table()
+    .provisionedThroughput()
+    .readCapacityUnits()
 
   val start = System.currentTimeMillis()
 
   val tokens = Source.tick(0 seconds, 5 seconds, NotUsed)   //Fréquence de refresh du throttle
-    .mapAsync(1)(_ => {
+    .mapMaterializedValue(_ => NotUsed)
+    .scanAsync(currentThrottle)((previousThrottle, _) => {
+      val describeF = DynamoDb.single(DescribeTableRequest.builder().tableName(tableName).build())
+      val currentUsageF = cloudWatch.getMetricData(GetMetricDataRequest
+        .builder()
+        .metricDataQueries(List(MetricDataQuery.builder() //TODO
+          .build()).asJava)
+        .build()
+      ).asScala
+
       for {
-        describe <- DynamoDb.single(DescribeTableRequest.builder().tableName(tableName).build())
+        describe <- describeF
+        currentUsage <- currentUsageF
         provisioned = describe.table().provisionedThroughput().readCapacityUnits()
-      } yield (0.8 * provisioned).toLong
+        consumedCapacity = currentUsage.metricDataResults().get(0).values().get(0) //TODO
+        otherUsage = consumedCapacity - previousThrottle
+        nextThrottle = ((0.8 * provisioned) - otherUsage).toLong
+      } yield nextThrottle
     })
     .expand(throttle => Iterator.continually(throttle))   //répète la valeur tant qu'il n'y en a pas de nouvelle
     .log("tokens", t => s"Throttle actuel ${t}/s")
     .throttle(1, 1 second)
+    .map(_.toLong)  //wtf
 
 
   //20 pendant les 20 premières secondes
